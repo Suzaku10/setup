290f21f88fc83404b121b68034022fb160ff4dd3
Suzaku10 <yudhistiray198@gmail.com>
2020-05-04 10:34:32 +0700

    add onselect and onrecieve in application

--- lib/application.dart
+++ lib/application.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:setup/setup.dart';
+
+typedef Future<void> OnSelectNotification(String payload);
+typedef Future<void> OnDidReceiveLocalNotification(
+    int id, String title, String body, String payload);
 
 class Application {
   static final Application _application = Application._internal();
   Completer<BuildContext> completer = Completer();
+  OnSelectNotification selectNotification;
+  OnDidReceiveLocalNotification receiveLocalNotificationIOS;
 
   factory Application() {
     return _application;
