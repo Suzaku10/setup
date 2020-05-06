cddc09f3287917f6c99772377d1f80f20c9cbd1f
Suzaku10 <yudhistiray198@gmail.com>
2020-05-06 08:48:38 +0700

    remove flutter local notification depedency

--- lib/application.dart
+++ lib/application.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 typedef Future<void> OnSelectNotification(String payload);
 typedef Future<void> OnDidReceiveLocalNotification(
@@ -11,7 +10,6 @@ class Application {
   Completer<BuildContext> completer = Completer();
   OnSelectNotification selectNotification;
   OnDidReceiveLocalNotification receiveLocalNotificationIOS;
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   factory Application() {
     return _application;
